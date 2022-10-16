7ef52074cf6c8af3b0fcc201b7579fd06d122ff7
Alexey <slow.net@mail.ru>
2022-10-16 20:22:25 +0300

    Задача со второго семинара

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+int randomNumber = GetRandomNumber(10, 100);
+
+Console.WriteLine(randomNumber);
+
+int max = randomNumber % 10;
+randomNumber = randomNumber / 10;
+
+while(randomNumber>0)
+{
+int LastNumber = randomNumber % 10;
+if(LastNumber>max)
+{
+max = LastNumber;
+}
+randomNumber = randomNumber /10;
+}
+
+Console.WriteLine(max);
+
+
+int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue,maxValue);
+    return randomNumber;
+}
